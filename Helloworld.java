d95e4626595f68eec1309b6b10fe8e650f6cf93e
ragulasneha <snehanitha2893@gmail.com>
2020-12-03 19:25:59 +0530

    updated helloworld

--- Helloworld.java
+++ Helloworld.java
@@ -1,5 +1,5 @@
-Public class HelloWorld{
-	public static void main(string[] args){
+public class HelloWorld {
+	public static void main(string[] args) {
 		System.out.println("Welcome to Helloworld");
 	}
 }
